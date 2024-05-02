b60c87f6e566ed5af9eefd8ab44dd3e5e37de0ac
brunofmoura <bruno.moura.iftm@gmail.com>
2024-05-02 15:03:37 -0300

    Criacao estrutura base camadas de controladores e servicos

--- src/main/java/com/example/demo/Repository/TaskRepository.java
+++ src/main/java/com/example/demo/Repository/TaskRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.Repository;
 
 import com.example.demo.Entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
